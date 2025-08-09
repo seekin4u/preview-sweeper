60b43ff40757500f516a483ddddaa651a34c8862
Max Sauce <wagamama47@gmail.com>
2025-08-09 12:32:05 +0300

    lib path fix

--- cmd/main.go
+++ cmd/main.go
@@ -25,22 +25,25 @@ import (
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
+	"github.com/seekin4u/preview-sweeper/internal/controller"
+	"k8s.io/apimachinery/pkg/runtime"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
-	"github.com/seekin4u/preview-sweeper/controllers"
-
-	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/certwatcher"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
+
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
-	// +kubebuilder:scaffold:imports
 )
+
+// +kubebuilder:scaffold:imports
 
 var (
 	scheme   = runtime.NewScheme()
@@ -201,7 +204,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	sweeper := &controllers.NamespaceSweeper{
+	sweeper := &controller.NamespaceSweeper{
 		Client: mgr.GetClient(),
 	}
 	sweeper.Start(ctrl.SetupSignalHandler(), 24*time.Hour)
